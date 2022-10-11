e08490ddd16c454f0b25c7d5004a532e7d4f28b9
阿沐 <babamu@126.com>
2022-10-11 11:14:01 +0800

    修复定时任务日志报错BUG

--- maku-cloud-module/maku-cloud-quartz/src/main/java/net/maku/quartz/entity/ScheduleJobEntity.java
+++ maku-cloud-module/maku-cloud-quartz/src/main/java/net/maku/quartz/entity/ScheduleJobEntity.java
@@ -1,11 +1,10 @@
 package net.maku.quartz.entity;
 
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import net.maku.framework.mybatis.entity.BaseEntity;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * 定时任务
@@ -13,24 +12,29 @@ import java.io.Serializable;
  * @author 阿沐 babamu@126.com
  */
 @Data
-@EqualsAndHashCode(callSuper=false)
 @TableName("schedule_job")
-public class ScheduleJobEntity extends BaseEntity implements Serializable {
+public class ScheduleJobEntity implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	* 任务名称
-	*/
+	 * id
+	 */
+	@TableId
+	private Long id;
+
+	/**
+	 * 任务名称
+	 */
 	private String jobName;
 
 	/**
-	* 任务组名
-	*/
+	 * 任务组名
+	 */
 	private String jobGroup;
 
 	/**
-	* bean名称
-	*/
+	 * bean名称
+	 */
 	private String beanName;
 
 	/**
@@ -39,28 +43,66 @@ public class ScheduleJobEntity extends BaseEntity implements Serializable {
 	private String method;
 
 	/**
-	* 方法参数
-	*/
+	 * 方法参数
+	 */
 	private String params;
 
 	/**
-	* cron表达式
-	*/
+	 * cron表达式
+	 */
 	private String cronExpression;
 
 	/**
-	* 状态 
-	*/
+	 * 状态
+	 */
 	private Integer status;
 
 	/**
-	* 是否并发  0：禁止  1：允许
-	*/
+	 * 是否并发  0：禁止  1：允许
+	 */
 	private Integer concurrent;
 
 	/**
-	* 备注
-	*/
+	 * 备注
+	 */
 	private String remark;
+
+	/**
+	 * 创建者
+	 */
+	@TableField(fill = FieldFill.INSERT)
+	private Long  creator;
+
+	/**
+	 * 创建时间
+	 */
+	@TableField(fill = FieldFill.INSERT)
+	private Date createTime;
+
+	/**
+	 * 更新者
+	 */
+	@TableField(fill = FieldFill.INSERT_UPDATE)
+	private Long  updater;
+
+	/**
+	 * 更新时间
+	 */
+	@TableField(fill = FieldFill.INSERT_UPDATE)
+	private Date updateTime;
+
+	/**
+	 * 版本号
+	 */
+	@Version
+	@TableField(fill = FieldFill.INSERT)
+	private Integer version;
+
+	/**
+	 * 删除标记
+	 */
+	@TableLogic
+	@TableField(fill = FieldFill.INSERT)
+	private Integer deleted;
 
 }

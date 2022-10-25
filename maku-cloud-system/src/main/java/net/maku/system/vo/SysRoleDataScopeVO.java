cfcab1138b25e5c4ec05f99cff7dc924af33f52a
阿沐 <babamu@126.com>
2022-10-25 14:58:16 +0800

    优化代码

--- maku-cloud-system/src/main/java/net/maku/system/vo/SysRoleDataScopeVO.java
+++ maku-cloud-system/src/main/java/net/maku/system/vo/SysRoleDataScopeVO.java
@@ -21,7 +21,7 @@ public class SysRoleDataScopeVO implements Serializable {
 	@NotNull(message = "角色ID不能为空")
 	private Long id;
 
-	@Schema(description = "数据范围  0：全部数据  1：本部门及子部门数据  2：本部门数据  3：本人数据  4：自定义数据")
+	@Schema(description = "数据范围  0：全部数据  1：本机构及子机构数据  2：本机构数据  3：本人数据  4：自定义数据")
 	@NotNull(message = "数据范围不能为空")
 	private Integer dataScope;
 
--- maku-cloud-system/src/main/java/net/maku/system/vo/SysRoleVO.java
+++ maku-cloud-system/src/main/java/net/maku/system/vo/SysRoleVO.java
@@ -30,7 +30,7 @@ public class SysRoleVO implements Serializable {
 	@Schema(description = "备注")
 	private String remark;
 
-	@Schema(description = "数据范围  0：全部数据  1：本部门及子部门数据  2：本部门数据  3：本人数据  4：自定义数据")
+	@Schema(description = "数据范围  0：全部数据  1：本机构及子机构数据  2：本机构数据  3：本人数据  4：自定义数据")
 	private Integer dataScope;
 
 	@Schema(description = "菜单ID列表")

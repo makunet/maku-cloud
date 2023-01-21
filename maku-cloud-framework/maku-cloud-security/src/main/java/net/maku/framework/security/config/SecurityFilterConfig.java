fac5e2a805ebec351f7725e6489653a0f2473fbd
阿沐 <babamu@126.com>
2023-01-21 11:13:34 +0800

    优化配置文件

--- maku-cloud-framework/maku-cloud-security/src/main/java/net/maku/framework/security/config/SecurityFilterConfig.java
+++ maku-cloud-framework/maku-cloud-security/src/main/java/net/maku/framework/security/config/SecurityFilterConfig.java
@@ -5,9 +5,10 @@ import net.maku.framework.security.exception.SecurityAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -23,7 +24,7 @@ import java.util.List;
 @Configuration
 @AllArgsConstructor
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity
 public class SecurityFilterConfig {
     private final OncePerRequestFilter authenticationTokenFilter;
     private final PermitResource permitResource;
@@ -37,13 +38,14 @@ public class SecurityFilterConfig {
         http
                 .addFilterBefore(authenticationTokenFilter, UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().authorizeRequests()
-                .antMatchers(permits).permitAll()
-                .antMatchers(HttpMethod.OPTIONS).permitAll()
-                .anyRequest().authenticated()
-                .and().exceptionHandling().authenticationEntryPoint(new SecurityAuthenticationEntryPoint())
+                .and().authorizeHttpRequests(auth -> auth
+                        .requestMatchers(permits).permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling().authenticationEntryPoint(new SecurityAuthenticationEntryPoint())
                 .and().headers().frameOptions().disable()
-                .and().csrf().disable()
+                .and().csrf(AbstractHttpConfigurer::disable)
         ;
 
         return http.build();
